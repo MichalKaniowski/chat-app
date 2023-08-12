af87cc4b8bf754e641e269a4d9e9b8fcaa1d731b
Michał Kaniowski <kaniowskimichal2@gmail.com>
2023-08-12 11:12:04 +0200

    Modify check-auth middleware

--- backend/api/middleware/check-auth.js
+++ backend/api/middleware/check-auth.js
@@ -1,14 +1,17 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-  const token = req.headers.authorization.split(" ")[1];
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
-  const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    req.userData = decoded;
 
-  if (decoded.role === "USER") {
-    return res.status(401).json({ message: "Unathorized." });
+    next();
+  } catch (error) {
+    res.status(400).json({
+      message:
+        "Invalid token or no token passed. Add authorization header with: Bearer <token>",
+    });
   }
-
-  req.userData = decoded;
-  next();
 };
